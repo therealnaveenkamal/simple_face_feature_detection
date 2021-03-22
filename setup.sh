ebf83d587864257bcca5a80fd93f81f2ed012249
Naveenraj Kamalakannan <therealnaveenkamal@gmail.com>
2021-03-22 17:58:53 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo “\
-
+echo "\
+[general]\n\
+email = \"naveenraj592000@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
-
 headless = true\n\
-
+enableCORS=false\n\
 port = $PORT\n\
-
-enableCORS = false\n\
-
-\n\
-
-” > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
