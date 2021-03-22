1e88353077b4253628e8729fed2249799511d139
Naveenraj Kamalakannan <therealnaveenkamal@gmail.com>
2021-03-22 18:35:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"naveenraj592000@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
